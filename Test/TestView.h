2034328b5ca4a04fca2f17d5c68e60d2eda2f2ed
Finn Gaida <f@fga.pw>
2016-06-20 11:46:31 +0200

    add CONTAINS_SWIFT flag to mute system settings whining

---
+++ Test/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  Test
+//
+//  Created by Finn Gaida on 20.06.16.
+//  Copyright © 2016 Makowiec. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface TestView : ScreenSaverView
+
+@end
